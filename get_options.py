b07e32deafd33985e84da29fd15620a67fccb5bd
William Sena <wllsena@protonmail.com>
2023-09-11 16:33:51 -0300

    Simplificando init_driver

--- get_options.py
+++ get_options.py
@@ -8,15 +8,11 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 def init_driver():
     options = ChromeOptions()
-    options.binary_location = "/opt/headless-chromium"
     options.add_argument("--headless")
     options.add_argument("--disable-gpu")
-    options.add_argument("--window-size=1280x1696")
-    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1280x800")
     options.add_argument("--no-sandbox")
-    options.add_argument("--remote-debugging-port=9222")
-    options.add_argument("--user-data-dir=/tmp/user-data")
-
+    
     service = Service(ChromeDriverManager().install())
 
     return webdriver.Chrome(service=service)
